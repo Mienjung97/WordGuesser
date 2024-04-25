d3e84b6dbd60da24c10b33039f7decf8792fed4c
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-04-25 08:33:49 +0000

    Moved helper functions into separate file

---
+++ helper.py
@@ -0,0 +1,38 @@
+import time
+
+# This file contains short functions that improve the game experience
+
+def print_line():
+    """
+    Add 79 dashes to form a line
+    """
+    print("_"*79, "\n")
+
+def pause(u):
+    """
+    Function to delay print statements adn takes a float as delay
+    in seconds.
+    """
+    time.sleep(u)
+
+def paused_print_05(print_txt):
+    """
+    Combines the pause (0.5s) and print functions for readability.
+    """
+    print(print_txt)
+    pause(0.5)
+
+def paused_print_1(print_txt):
+    """
+    Combines the pause (1s) and print functions for readability.
+    """
+    print(print_txt)
+    pause(1)
+
+
+def paused_print_2(print_txt):
+    """
+    Combines the pause (2s) and print functions for readability.
+    """
+    print(print_txt)
+    pause(2)
--- run.py
+++ run.py
@@ -1,46 +1,14 @@
 import random
-import time
+
 import re
 from words import word_list, MAX_TRIES
 from art import print_ascii
+from helper import print_line, pause, paused_print_05, paused_print_1, paused_print_2
 
 guessed_letters = []
 game_counter = 1
 
 
-def print_line():
-    """
-    Add 79 dashes to form a line
-    """
-    print("_"*79, "\n")
-
-def pause(u):
-    """
-    Function to delay print statements adn takes a float as delay
-    in seconds.
-    """
-    time.sleep(u)
-
-def paused_print_1(print_txt):
-    """
-    Combines the pause (1s) and print functions for readability.
-    """
-    print(print_txt)
-    pause(1)
-
-def paused_print_05(print_txt):
-    """
-    Combines the pause (0.5s) and print functions for readability.
-    """
-    print(print_txt)
-    pause(0.5)
-
-def paused_print_2(print_txt):
-    """
-    Combines the pause (2s) and print functions for readability.
-    """
-    print(print_txt)
-    pause(2)
 
 def take_user_name_input():
     """
